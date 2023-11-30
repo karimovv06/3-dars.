17b17f56abbff9b4c75fb640766aa1e7c4c927a1
unknown <z5304989@gmail.com>
2023-11-30 11:41:32 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+                                                            # 1-mashq
+besh = int(input("juft sonni kiriting: "))
+
+if besh % 2 == 0:
+  print("to'g'ri")
+else:
+  print("bu juft son emas")
+
+                                                        # 2-mashq 1 chisi
+
+x = int(input("Yoshingizni kiriting: "))
+if x <4 or x >61:
+    print("Tekin")
+else:
+    print("To'lash kerak")
+
+                                                             # 2 chisi
+
+y = int(input("Yoshingizni kiriting: "))
+
+if y <18:
+    print("10000 ming so'm")
+else:
+    print("To'lash kerak o'n ming")
+
+                                                              #3 chisi
+
+m = int(input("Yoshingizni kiriting: "))
+
+if m >18:
+    print("20000 ming so'm")
+else:
+    print("kichkinasan")
+                                                            # 3-mashq
+
+    b = int(input("sonni kiriting: "))
+    son = n + 1
+    print(f"{n} dan keyin {keyingi_son} keladi")
+
+
+
+
+
+
+
+
